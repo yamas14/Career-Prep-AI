9cc336cb66dae3d17bc48f2bfce85ece447c6062
Yamas14 <saisamay80@gmail.com>
2025-04-06 14:39:02 +0530

    Updated vite config and pushed new changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Career-Prep-AI',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
